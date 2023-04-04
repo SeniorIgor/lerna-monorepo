7610bdd5e94edeb349c303fdcdfed1bfa835fadc
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2023-04-04 22:22:17 +0600

    create second package

---
+++ packages/core-utils/__tests__/index.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const coreUtils = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(coreUtils(), 'Hello from coreUtils');
+console.info('coreUtils tests passed');
---
+++ packages/core-utils/lib/index.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = coreUtils;
+
+function coreUtils() {
+  return 'Hello from coreUtils';
+}
