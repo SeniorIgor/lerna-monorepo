b5d9b1533d9055ed128ac5cf85fa40dfc67728bd
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2023-04-06 16:50:54 +0600

    feat: optimize packages

--- packages/core/__tests__/index.test.js
+++ packages/core/__tests__/index.test.js
@@ -1,7 +1,9 @@
-const { isOdd } = require("../dist/index.js");
+const { isOdd } = require('../dist/index.js');
 
-test("isOdd", () => {
+test('isOdd', () => {
   expect(isOdd(1)).toBe(true);
   expect(isOdd(2)).toBe(false);
   expect(isOdd(3)).toBe(true);
+  expect(isOdd(4)).toBe(false);
+  expect(isOdd(5)).toBe(true);
 });
