9b09c0bc69f4eb87b6f7c4fdc58f801846fba747
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2023-04-07 13:12:24 +0600

    fix(core): change func

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -5,3 +5,7 @@ export function isOdd(n: number): boolean {
 export function someFunction(n: number): boolean {
   return n % 2 === 1;
 }
+
+export function someFunction1(n: number): boolean {
+  return n % 2 === 1;
+}
