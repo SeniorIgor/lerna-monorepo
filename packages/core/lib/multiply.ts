550c626f4ea9b7392f70b14406e05c8f7657dbc9
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2023-04-18 17:49:49 +0600

    feat: delete some func

--- packages/core/lib/multiply.ts
+++ packages/core/lib/multiply.ts
@@ -5,7 +5,3 @@ export function multiply(first: number, second: number): number {
 export function multiply2(first: number, second: number): number {
   return first * second;
 }
-
-export function multiply3(first: number, second: number): number {
-  return first * second;
-}
