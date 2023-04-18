3626e608de8ae14c32a33700eb2681d6f2520167
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2023-04-18 17:12:47 +0600

    fix(core): fix func name

--- packages/core/lib/multiply.ts
+++ packages/core/lib/multiply.ts
@@ -2,6 +2,6 @@ export function multiply(first: number, second: number): number {
   return first * second;
 }
 
-export function multiply1(first: number, second: number): number {
+export function multiply2(first: number, second: number): number {
   return first * second;
 }
